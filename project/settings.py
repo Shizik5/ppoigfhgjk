841dce12495be760bdb1fdd89006bdb97df912b7
Darina Ibragimova <darinaibragimova@Darina.local>
2023-07-28 14:01:08 +0600

    f

--- project/settings.py
+++ project/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'hackaton',
-        'USER': 'alina',
+        'USER': 'darinaibragimova',
         'PASSWORD': '1',
         'HOST': '127.0.0.1',
         'PORT': '5432'
